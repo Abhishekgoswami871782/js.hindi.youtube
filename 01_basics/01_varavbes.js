d175b17c6376580d869c7a01ed22c43786420821
Abhishek puri goswami <nicky871782@gmail.com>
2025-03-22 13:53:43 +0000

    01_varavbes.js

---
+++ 01_basics/01_varavbes.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "nicky871782@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountState;
+
+
+//accountId = "2" 
+accountEmail = "hc@c.com"
+accountPassword = "212121"
+accountCite = "Bengaluru";
+
+
+console.log(accountId);
+
+/* Prefer not to use var because of issue in block scope and functional scope*/ 
+
+console.log([accountId, accountEmail,accountPassword,accountCite,accountState])
