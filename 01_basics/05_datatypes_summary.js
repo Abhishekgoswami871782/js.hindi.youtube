20c801f0ac060120ec86ef02ea306ddfdd20181e
Abhishek puri goswami <nicky871782@gmail.com>
2025-04-02 14:52:29 +0000

    comparisionOperation.js/datatypes_summary.js

---
+++ 01_basics/05_datatypes_summary.js
@@ -0,0 +1,33 @@
+//primitive datatypes
+
+//7types  String,numbar. boolean .tring. Symbol.  BigInt, null.
+
+
+consol scor = 100
+consot scoreValue = 100.3
+
+const isLaggedIn = false
+const outsideTemp = null
+let userEmail;
+
+ 
+//Rference (non primitive)
+ 
+//Array. objects, Functions
+ 
+ 
+const heros = ["shaktiman", "naagraj", "doga"];
+
+let MyObj = {
+
+    name: "hitesh",
+    age: 22,
+
+}
+
+cosnt myFunction = function (){
+
+    console.log("hello world")
+}
+
+console.log(typeof bigNumbe);
