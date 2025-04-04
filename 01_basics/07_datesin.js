c541c034d22444838485c1ca6f0f7eec37bfd035
Abhishek puri goswami <nicky871782@gmail.com>
2025-04-04 13:26:59 +0000

    07_datasin.js

---
+++ 01_basics/07_datesin.js
@@ -0,0 +1,40 @@
+//Date
+
+
+let myDate = new Date()
+
+// console.log(myDate.toString());
+// //Fri Apr 04 2025 09:05:57 GMT+0000 (Coordinated Universal Time)
+// console.log(myDate.toDateString());
+// //fri Apr 04 2025
+// console.log(myDate.toLocaleString());
+// //4/4/2025
+// console.log(typeof myDate);
+// //object
+
+//let myCreatedDate = new Date(2023, 0, 23)
+
+//console.log(myCreatedDate. toDateString());
+//mon jan 23 2023
+
+//let myCreatedDate = new Date ("01-04-2025")
+
+//console.log(myCreatedDate.toLocaleString());
+//let myTimeStamp = Date.now();
+    // console.log(myTimeStamp);
+
+    // console.log(myCreatedDate.getTime());
+
+    //console.log(Date.now());
+
+let newDate = new Date()
+  console.log(newDate);
+  console.log(newDate.getMonth());
+  console.log(newDate.getday());
+
+
+ // `${newDate.getDay()} and the time `
+
+ newDate.toLocaleString('default')
+
+    weekday: "long"
